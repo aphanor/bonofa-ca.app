bcf4b86add0713dd8f2c7d6d900f779319803825
Alexis <alexis.phanor@gmail.com>
2015-07-31 16:56:37 +0100

    Fixed scroll display issue for languages - Alexis

--- build/js/scroll.js
+++ build/js/scroll.js
@@ -4,12 +4,14 @@ $(window).scroll(function(){
     var logo_v = $('.logo img');
     var section = $('section.nav');
     var color_links = $('ul li a');
+    var languages = $('div.languages');
     
     var scrollPos = $(this).scrollTop();
     
 	if (scrollPos === 0) {
     	
-		
+    	console.log('Club going up on a Tuesday!');
+    	
 	} else if (scrollPos < 415) {
     	
     	section.removeClass('effecto');
@@ -31,6 +33,17 @@ $(window).scroll(function(){
         section.removeClass('effecta').addClass('effectu');
         nav_bar.addClass('effectu-top');
         logo_v.addClass('effectu-logo');
+    }
+
+	switch (true) {
+        case (scrollPos < 300):
+            languages.fadeIn(1000);
+            break;
+        case (scrollPos > 410):
+           languages.fadeOut(800);
+            break;
+        default:
+            languages.show();
     }
     
 });
